71cd822cccf94ff16bf1f79c0bd73812b79dc862
Muhamad Nuralam <136845377+muhamadnuralam@users.noreply.github.com>
2023-08-20 20:51:44 +0700

    Update login-script.js
    ubah

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -3,6 +3,12 @@
  * Gunakan fungsi goToHome() untuk menampilkan halaman home
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+ 
+ /*
+ * Catatan:
+ * Gunakan fungsi goToHome() untuk menampilkan halaman home
+ * Gunakan fungsi goToLogin() untuk menampilkan halaman login
+ * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
 const loginFormElement = document.querySelector('#loginForm');
